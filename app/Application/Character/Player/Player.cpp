c70eb57f138f185d33dc22c97c711368ec2a572c
Mutuking <kawaguchi.2525.haruki@gmail.com>
2026-09-01 16:16:38 +0900

    プレイヤーの基本動作の雛形を作りました

--- app/Application/Character/Player/Player.cpp
+++ app/Application/Character/Player/Player.cpp
@@ -6,6 +6,41 @@ void Player::Init(const std::string objectName) {
 }
 
 void Player::Update() {
+	if (inputContext_.move) {
+		Hagine::Vector3 translate = transform_->translation_;
+		translate.x += inputContext_.dir.x * 0.1f;
+		translate.z += inputContext_.dir.y * 0.1f;
+		transform_->translation_ = translate;
+	}
+
+	if (inputContext_.jump) {
+		Hagine::Vector3 translate = transform_->translation_;
+		translate.y += 0.1f;
+		transform_->translation_ = translate;
+		isJumping_ = true;
+	}
+
+	if (!inputContext_.jump && isJumping_) {
+		Hagine::Vector3 translate = transform_->translation_;
+		translate.y -= 0.1f;
+		transform_->translation_ = translate;
+		if (translate.y <= 0.0f) {
+			translate.y = 0.0f;
+			isJumping_ = false;
+		}
+	}
+
+	if (inputContext_.attack) {
+		// 攻撃処理
+	}
+
+	if (inputContext_.dash) {
+		Hagine::Vector3 translate = transform_->translation_;
+		translate.x += inputContext_.dir.x * 0.5f;
+		translate.z += inputContext_.dir.y * 0.5f;
+		transform_->translation_ = translate;
+	}
+
 	BaseObject::Update();
 }
 
---
+++ app/Application/Input/GameInput.cpp
@@ -0,0 +1,40 @@
+#include "GameInput.h"
+#include "Input/Input.h"
+
+void GameInput::UpdateInputState() {
+	auto input = Hagine::Input::GetInstance();
+
+	context_.move = false;
+	context_.dir = Hagine::Vector2{0.0f, 0.0f};
+	if (input->PushKey(DIK_W)) {
+		context_.move = true;
+		context_.dir.y += 1.0f;
+	} if (input->PushKey(DIK_S)) {
+		context_.move = true;
+		context_.dir.y -= 1.0f;
+	} if (input->PushKey(DIK_A)) {
+		context_.move = true;
+		context_.dir.x -= 1.0f;
+	} if (input->PushKey(DIK_D)) {
+		context_.move = true;
+		context_.dir.x += 1.0f;
+	}
+
+	if (input->PushKey(DIK_SPACE)) {
+		context_.jump = true;
+	} else {
+		context_.jump = false;
+	}
+
+	if (input->PushKey(DIK_J)) {
+		context_.attack = true;
+	} else {
+		context_.attack = false;
+	}
+
+	if (input->PushKey(DIK_K)) {
+		context_.dash = true;
+	} else {
+		context_.dash = false;
+	}
+}
